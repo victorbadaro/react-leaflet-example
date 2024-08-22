022f8261ec239e4900e4b984355809a59064d389
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-08-22 02:27:17 -0300

    feat: add a basic working map

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,23 @@
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
 export function App() {
-	return <h1>Hello, World!</h1>;
+	return (
+		<MapContainer
+			center={[0, 0]}
+			zoom={3}
+			style={{
+				height: '1000px'
+			}}
+		>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+			<Marker position={[0, 0]}>
+				<Popup>
+					A pretty CSS3 popup. <br /> Easily customizable.
+				</Popup>
+			</Marker>
+		</MapContainer>
+	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './app.tsx';
 
+import 'leaflet/dist/leaflet.css';
 import './index.css';
 
 const container = document.getElementById('root');
